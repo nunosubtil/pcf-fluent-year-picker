2a2974e3bf59ea2a0656b622219623bf78adbddd
nunosubtil <150437999+nunosubtil@users.noreply.github.com>
2024-08-11 19:01:05 +0100

    added formFactor for responsive behavior

--- FluentYearPicker/index.ts
+++ FluentYearPicker/index.ts
@@ -3,10 +3,10 @@ import FluentYearPicker, { FluentYearPickerProps } from "./FluentYearPicker";
 import * as React from "react";
 
 export class PCFFluentYearPicker implements ComponentFramework.ReactControl<IInputs, IOutputs> {
-    private theComponent!: ComponentFramework.ReactControl<IInputs, IOutputs>;
     private notifyOutputChanged!: () => void;
     private _value!: string;
     private _isDarkMode!: boolean;
+    private _formFactor!: number;
 
     constructor() { }
 
@@ -16,19 +16,28 @@ export class PCFFluentYearPicker implements ComponentFramework.ReactControl<IInp
         state: ComponentFramework.Dictionary
     ): void {
         this.notifyOutputChanged = notifyOutputChanged;
-        this._isDarkMode = context.fluentDesignLanguage?.isDarkTheme ?? false; // Check theme from the context
-        this._value = context.parameters.yearInput.raw || ""; 
+        this._value = context.parameters.yearInput.raw || "";
+        this._updateContextValues(context);
     }
 
     public updateView(context: ComponentFramework.Context<IInputs>): React.ReactElement {
+        // Update values in case they change dynamically
+        this._updateContextValues(context);
+
         const props: FluentYearPickerProps = { 
             year: context.parameters.yearInput.raw || this._value,
             updatedValue: this._updateValue.bind(this),
-            isDarkMode: this._isDarkMode
+            isDarkMode: this._isDarkMode,
+            formFactor: this._formFactor
         };
         return React.createElement(
             FluentYearPicker, props
         );
+    }
+
+    private _updateContextValues(context: ComponentFramework.Context<IInputs>): void {
+        this._isDarkMode = context.fluentDesignLanguage?.isDarkTheme ?? false; // Handle theme dynamically
+        this._formFactor = context.client.getFormFactor(); // Handle form factor dynamically
     }
     
     private _updateValue(value: string) {
